6827468776ea7322ea7463968ad55ce181e417a4
Greg Stephen <GregAStephen@gmail.com>
2020-06-14 18:44:04 -0500

    added correct guess and action types

---
+++ src/actions/index.test.js
@@ -0,0 +1,10 @@
+import { correctGuess, actionType, actionTypes } from './index';
+
+describe('correctGuess', () => {
+  test('returns an action with type `CORRECT_GUESS`', () => {
+    const action = correctGuess();
+    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+  });
+});
+
+
