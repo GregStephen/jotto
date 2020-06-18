68c9028609501873843199a333f8dccaa54abf8b
Greg Stephen <GregAStephen@gmail.com>
2020-06-17 23:55:02 -0500

    added input component and testing and store creator

--- Test/testUtils.js
+++ Test/testUtils.js
@@ -1,6 +1,19 @@
 /* eslint-disable react/forbid-foreign-prop-types */
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
 
+import rootReducer from '../src/reducers';
+
+/**
+ * Crate a testing store with imported reducers, middleware and initial state.
+ *  globals: rootReducer. 
+ * @param {object} initialState - Initial state for store.
+ * @function storeFactory
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
 /**
  * Return node(s) with the given data-test attribute
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
